6a1899d075da3eae72d05b16e6d949af77fb8f8c
TokiRoko <severinomanfury07@gmail.com>
2024-01-16 16:04:40 +0800

    Second Commit

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu(){
     menu.classList.toggle("open");  
     icon.classList.toggle("open");  
 }
+
+const faders = document.querySelectorAll('.section__pic-container-1');
+const faders1 = document.querySelectorAll('.section__text');
+const appearOptions = {
+    threshold: 1
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries, 
+    appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting){
+                return;
+            }else{
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    }, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+faders1.forEach(fader1 => {
+    appearOnScroll.observe(fader1);
+})
